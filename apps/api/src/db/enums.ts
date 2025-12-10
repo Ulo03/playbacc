df2c1f09f24a7f18309c8b330b02448d68a372b8
Lukas Ulovec <uloveclukas@gmail.com>
2025-12-10 19:38:08 +0100

    🗃️ Add custom types

---
+++ apps/api/src/db/enums.ts
@@ -0,0 +1,7 @@
+import { pgEnum } from "drizzle-orm/pg-core";
+
+// User Role Enum
+export const userRoleEnum = pgEnum("user_role", ["admin", "user"]);
+
+// Account Provider Enum
+export const accountProviderEnum = pgEnum("account_provider", ["spotify"]);
