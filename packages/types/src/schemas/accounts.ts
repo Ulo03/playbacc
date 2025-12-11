1c991b4b66f11959773fafdba70fa7872e2704db
Lukas Ulovec <uloveclukas@gmail.com>
2025-12-11 16:13:04 +0100

    🗃️ Update zod schemas

--- packages/types/src/schemas/accounts.ts
+++ packages/types/src/schemas/accounts.ts
@@ -6,7 +6,7 @@ export const accountSchema = z.object({
     id: z.uuid(),
     user_id: z.uuid(),
     provider: accountProviderSchema,
-    provider_account_id: z.string(),
+    external_id: z.string(),
     access_token: z.string().nullish(),
     refresh_token: z.string().nullish(),
     expires_at: z.number().nullish(),
--- packages/types/src/schemas/scrobbles.ts
+++ packages/types/src/schemas/scrobbles.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { accountProviderSchema } from "./enums";
 
 // Scrobble Schema
 export const scrobbleSchema = z.object({
@@ -7,8 +8,10 @@ export const scrobbleSchema = z.object({
     track_id: z.uuid(),
     album_id: z.uuid().nullish(),
     played_at: z.iso.datetime({ offset: true }),
-    played_duration_ms: z.number().nullish(),
-    skipped: z.boolean().nullish(),
+    played_duration_ms: z.number(),
+    skipped: z.boolean().default(false),
+    is_imported: z.boolean().default(false),
+    provider: accountProviderSchema,
 });
 
 // Scrobble Type
